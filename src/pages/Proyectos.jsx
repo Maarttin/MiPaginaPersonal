4853ffd64c25273cb3bddca0b93f4de96f2cb310
Martín <martinaragonsanchez@gmail.com>
2026-03-10 01:39:51 -0600

    ajuste de estilos en proyectos

--- src/pages/Proyectos.jsx
+++ src/pages/Proyectos.jsx
@@ -11,26 +11,26 @@ function Proyectos() {
                     <br />
                     <li>
                         <h2>Administración para una biblioteca</h2>
-                        <p><a href="https://github.com/Maarttin/bibliotecaInventario" target="_blank" rel="noopener noreferrer">Ir al repositorio</a></p>
+                        <p className="enlace"><a href="https://github.com/Maarttin/bibliotecaInventario" target="_blank" rel="noopener noreferrer">Ir al repositorio</a></p>
                         <img src={Bi} alt="Proyecto 1" className="proyecto-imagen" />
                     </li>
                      <br />
                     <li>
                         <h2>Sistema para reseñar libros y peliculas</h2>
-                        <p><a href="https://github.com/Maarttin/CineCritico" target="_blank" rel="noopener noreferrer">Ir al repositorio</a></p>
+                        <p className="enlace"><a href="https://github.com/Maarttin/CineCritico" target="_blank" rel="noopener noreferrer">Ir al repositorio</a></p>
                         <img src={Cine} alt="Proyecto 2" className="proyecto-imagen" />
 
                     </li>
                       <br />
                     <li>
                         <h2>Mi propia pagina personal</h2>
-                        <p><a href="https://github.com/Maarttin/MiPaginaPersonal" target="_blank" rel="noopener noreferrer">Ir al repositorio</a></p>
+                        <p className="enlace"><a href="https://github.com/Maarttin/MiPaginaPersonal" target="_blank" rel="noopener noreferrer">Ir al repositorio</a></p>
                         <img src={Personal} alt="Proyecto 3" className="proyecto-imagen" />
                     </li>
                     <br />
                     <li>
                         <h2>Simulador de finanzas</h2>
-                        <p><a href="https://github.com/Maarttin/finanzasDesk" target="_blank" rel="noopener noreferrer">Ir al repositorio</a></p>
+                        <p className="enlace"><a href="https://github.com/Maarttin/finanzasDesk" target="_blank" rel="noopener noreferrer">Ir al repositorio</a></p>
                     </li>
                 </ul>
             </div>
